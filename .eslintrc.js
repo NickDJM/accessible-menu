b05128c1f90ee662a214cbc9580d825bc5388c8e
Nick Milton <nickdjm@gmail.com>
2019-11-16 19:53:09 -0500

    build(npm): implement eslint and hook
    See #3

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    "standard",
+    "plugin:jsdoc/recommended",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["prettier", "jsdoc"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {}
+};
