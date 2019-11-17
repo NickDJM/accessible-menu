9bf6c80a4de8fcd8e39e5bc233112b21b68746cb
Nick Milton <nickdjm@gmail.com>
2019-11-16 19:03:43 -0500

    build(npm): add commitlint and hook

--- .huskyrc.js
+++ .huskyrc.js
@@ -4,6 +4,7 @@ module.exports = {
   hooks: {
     "pre-commit": tasks([
       "echo committing as $(git config user.name)"
-    ])
+    ]),
+    "commit-msg": "commitlint -E HUSKY_GIT_PARAMS"
   }
 };
