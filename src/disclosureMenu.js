abfeb57fc0b9f602d7bd5abfb12429fce63aef94
Nick Milton <nickdjm@gmail.com>
2020-04-23 13:59:22 -0400

    fix(menu): add default null values to subclasses

--- src/disclosureMenu.js
+++ src/disclosureMenu.js
@@ -7,21 +7,21 @@ class DisclosureMenu extends BaseMenu {
   /**
    * {@inheritdoc}
    *
-   * @param {object}         param0                               - The menu object.
-   * @param {HTMLElement}    param0.menuElement                   - The menu element in the DOM.
-   * @param {string}         [param0.menuItemSelector = "li"]     - The CSS selector string for menu items.
-   * @param {string}         [param0.menuLinkSelector = "a"]      - The CSS selector string for menu links.
-   * @param {string}         [param0.submenuItemSelector = ""]    - The CSS selector string for menu items containing submenus.
-   * @param {string}         [param0.submenuToggleSelector = "a"] - The CSS selector string for submenu toggle buttons/links.
-   * @param {string}         [param0.submenuSelector = "ul"]      - The CSS selector string for submenus.
-   * @param {HTMLElement}    param0.controllerElement             - The element controlling the menu in the DOM.
-   * @param {HTMLElement}    param0.containerElement              - The element containing the menu in the DOM.
-   * @param {string}         [param0.openClass = "show"]          - The class to apply when a menu is "open".
-   * @param {string}         [param0.closeClass = "hide"]         - The class to apply when a menu is "closed".
-   * @param {boolean}        [param0.isTopLevel = false]          - A flag to mark the root menu.
-   * @param {DisclosureMenu} param0.parentMenu                    - The parent menu to this menu.
-   * @param {boolean}        [param0.isHoverable = false]         - A flag to allow hover events on the menu.
-   * @param {number}         [param0.hoverDelay = 250]            - The delay for closing menus if the menu is hoverable (in miliseconds).
+   * @param {object}              param0                               - The menu object.
+   * @param {HTMLElement}         param0.menuElement                   - The menu element in the DOM.
+   * @param {string}              [param0.menuItemSelector = "li"]     - The CSS selector string for menu items.
+   * @param {string}              [param0.menuLinkSelector = "a"]      - The CSS selector string for menu links.
+   * @param {string}              [param0.submenuItemSelector = ""]    - The CSS selector string for menu items containing submenus.
+   * @param {string}              [param0.submenuToggleSelector = "a"] - The CSS selector string for submenu toggle buttons/links.
+   * @param {string}              [param0.submenuSelector = "ul"]      - The CSS selector string for submenus.
+   * @param {HTMLElement|null}    [param0.controllerElement = null]    - The element controlling the menu in the DOM.
+   * @param {HTMLElement|null}    [param0.containerElement = null]     - The element containing the menu in the DOM.
+   * @param {string}              [param0.openClass = "show"]          - The class to apply when a menu is "open".
+   * @param {string}              [param0.closeClass = "hide"]         - The class to apply when a menu is "closed".
+   * @param {boolean}             [param0.isTopLevel = false]          - A flag to mark the root menu.
+   * @param {DisclosureMenu|null} [param0.parentMenu = null]           - The parent menu to this menu.
+   * @param {boolean}             [param0.isHoverable = false]         - A flag to allow hover events on the menu.
+   * @param {number}              [param0.hoverDelay = 250]            - The delay for closing menus if the menu is hoverable (in miliseconds).
    */
   constructor({
     menuElement,
@@ -30,12 +30,12 @@ class DisclosureMenu extends BaseMenu {
     submenuItemSelector = "",
     submenuToggleSelector = "a",
     submenuSelector = "ul",
-    controllerElement,
-    containerElement,
+    controllerElement = null,
+    containerElement = null,
     openClass = "show",
     closeClass = "hide",
     isTopLevel = true,
-    parentMenu,
+    parentMenu = null,
     isHoverable = false,
     hoverDelay = 250,
   }) {
