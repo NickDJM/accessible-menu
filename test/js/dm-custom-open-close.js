6fd339671a3ca151ef1042e7580b01b1a359e24a
Nick Milton <nickdjm@gmail.com>
2020-06-16 22:34:12 -0400

    test(overhaul): simplify test files

---
+++ test/js/dm-custom-open-close.js
@@ -0,0 +1,18 @@
+const navs = document.querySelectorAll("nav");
+const menus = [];
+
+Array.from(navs).forEach(nav => {
+  const menuElement = nav.querySelector("ul");
+  const submenuItemSelector = "li.dropdown";
+  const controllerElement = nav.id === "main-menu" ? nav.querySelector("button") : null;
+  const containerElement = nav.id === "main-menu" ? nav : null;
+
+  menus.push(new AccessibleMenu.Menubar({
+    menuElement,
+    submenuItemSelector,
+    controllerElement,
+    containerElement,
+    openClass: ["show", "open"],
+    closeClass: "",
+  }));
+});
