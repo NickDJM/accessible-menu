97eec5b4bea3a905b69438e77df55942713eb111
Tyler Elwing <telwing@vividfront.com>
2021-02-04 17:00:31 -0500

    build(polyfills): add polyfill for Symbol

--- src/rollup.js
+++ src/rollup.js
@@ -3,6 +3,7 @@ import "./polyfills/array.includes.polyfill.js";
 import "./polyfills/array.find.polyfill.js";
 import "./polyfills/string.startsWith.polyfill.js";
 import "./polyfills/string.endsWith.polyfill.js";
+import "./polyfills/symbol.polyfill.js";
 import Menubar from "./menubar.js";
 import DisclosureMenu from "./disclosureMenu.js";
 
