053eae06e7f1a5194e111c774c4b84a17972a57d
Nick Milton <nickdjm@gmail.com>
2021-05-06 14:56:56 -0400

    chore: remove reference to open/close class

--- src/disclosureMenuToggle.js
+++ src/disclosureMenuToggle.js
@@ -13,8 +13,6 @@ class DisclosureMenuToggle extends BaseMenuToggle {
    * @param {HTMLElement}          param0.menuToggleElement     - The toggle element in the DOM.
    * @param {HTMLElement}          param0.parentElement         - The element containing the controlled menu.
    * @param {DisclosureMenu}       param0.controlledMenu        - The menu controlled by this toggle.
-   * @param {string|string[]|null} [param0.openClass = "show"]  - The class to apply when the controlled menu is "open".
-   * @param {string|string[]|null} [param0.closeClass = "hide"] - The class to apply when the controlled menu is "closed".
    * @param {DisclosureMenu|null}  [param0.parentMenu = null]   - The menu containing this toggle.
    */
   constructor({
