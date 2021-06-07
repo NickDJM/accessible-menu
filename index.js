b168dd3ce6666141b8a83ad8ecb6cf9c733ebedd
Nick Milton <nickdjm@gmail.com>
2021-06-07 14:38:28 -0400

    feat(menu): allow Menubar and DisclosureMenu to be imported individually

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import Menubar from "./src/menubar";
 import DisclosureMenu from "./src/disclosureMenu";
 
+const menubar = Menubar;
+const disclosureMenu = DisclosureMenu;
+
+export { disclosureMenu as DisclosureMenu };
+export { menubar as Menubar };
 export default {
   Menubar,
   DisclosureMenu,
