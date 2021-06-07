be89706a7c01eb508074c42c223a18be1d7014dd
Nick Milton <nickdjm@gmail.com>
2021-06-07 14:34:11 -0400

    build: overhaul build and development config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,13 @@
 module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
   trailingComma: "es5",
-  arrowParens: "avoid",
+  bracketSpacing: true,
+  arrowParens: "always",
+  htmlWhitespaceSensitivity: "css",
+  vueIndentScriptAndStyle: false,
+  endOfLine: "lf",
 };
--- .versionrc.js
+++ .versionrc.js
@@ -1,58 +1,59 @@
 module.exports = {
-  types: [{
+  types: [
+    {
       type: "feat",
       section: "Features",
-      hidden: false
+      hidden: false,
     },
     {
       type: "fix",
       section: "Bug Fixes",
-      hidden: false
+      hidden: false,
     },
     {
       type: "perf",
       section: "Performance Improvements",
-      hidden: false
+      hidden: false,
     },
     {
       type: "revert",
       section: "Reverts",
-      hidden: false
+      hidden: false,
     },
     {
       type: "docs",
       section: "Documentation",
-      hidden: false
+      hidden: false,
     },
     {
       type: "style",
       section: "Styles",
-      hidden: true
+      hidden: true,
     },
     {
       type: "chore",
       section: "Miscellaneous Chores",
-      hidden: true
+      hidden: true,
     },
     {
       type: "refactor",
       section: "Code Refactoring",
-      hidden: false
+      hidden: false,
     },
     {
       type: "test",
       section: "Tests",
-      hidden: true
+      hidden: true,
     },
     {
       type: "build",
       section: "Build System",
-      hidden: false
+      hidden: false,
     },
     {
       type: "ci",
       section: "Continuous Integration",
-      hidden: false
-    }
-  ]
-}
+      hidden: false,
+    },
+  ],
+};
