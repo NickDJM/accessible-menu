ca2d7dc0fa7019d8cb82d460901454f02a892715
Nick Milton <nickdjm@gmail.com>
2021-06-17 15:21:10 -0400

    test(menus): add initialization tests

--- tests/menus/_baseMenu.test.js
+++ tests/menus/_baseMenu.test.js
@@ -7,6 +7,16 @@
 import BaseMenu from "../../src/_baseMenu";
 import { oneLevelMenu } from "../test-menus";
 
+/**
+ * Initializes a BaseMenu and it's children since the menu will not auto-initialize.
+ *
+ * @param {BaseMenu} menu - The menu to initialize.
+ */
+function initializeMenu(menu) {
+  menu.initialize();
+  menu.elements.menuItems.forEach((menuItem) => menuItem.initialize());
+}
+
 describe("BaseMenu single-level menu sanity check", () => {
   // Mock console.error.
   console.error = jest.fn((error) => {
@@ -24,8 +34,7 @@ describe("BaseMenu single-level menu sanity check", () => {
 
   test("initializes", () => {
     expect(() => {
-      menu.initialize();
-      menu.elements.menuItems.forEach((menuItem) => menuItem.initialize());
+      initializeMenu(menu);
     }).not.toThrow(Error);
   });
 
@@ -129,5 +138,257 @@ describe("BaseMenu single-level menu sanity check", () => {
 
   test("should not focus", () => {
     expect(menu.shouldFocus).toBeFalse();
+  });
+});
+
+describe("BaseMenu initialization tests", () => {
+  // Mock console.error.
+  console.error = jest.fn((error) => {
+    throw new Error(error.message);
+  });
+
+  // Set up the DOM.
+  document.body.innerHTML = oneLevelMenu;
+  const menuElement = document.querySelector("#menu-0");
+  const containerElement = document.querySelector("nav");
+  const controllerElement = document.querySelector("#toggle-0");
+
+  // CSS selectors for testing.
+  const basicCSSSelectors = [
+    "menuItemSelector",
+    "menuLinkSelector",
+    "submenuItemSelector",
+  ];
+
+  // Class lists for testing.
+  const classLists = ["openClass", "closeClass"];
+
+  test("won't initialize if nothing is passed", () => {
+    expect(() => {
+      const menu = new BaseMenu();
+
+      initializeMenu(menu);
+    }).toThrow("Cannot read property 'menuElement' of undefined");
+  });
+
+  test("won't initialize if no menuElement is passed", () => {
+    expect(() => {
+      const menu = new BaseMenu({});
+
+      initializeMenu(menu);
+    }).toThrow(
+      "AccessibleMenu: menuElement must be an instance of HTMLElement. undefined given."
+    );
+  });
+
+  test("won't initialize if menuElement isn't an HTMLElement", () => {
+    expect(() => {
+      const menu = new BaseMenu({ menuElement: {} });
+
+      initializeMenu(menu);
+    }).toThrow(
+      "AccessibleMenu: menuElement must be an instance of HTMLElement. object given."
+    );
+  });
+
+  test.each(basicCSSSelectors)(
+    "won't initialize if %s isn't a CSS selector",
+    (selector) => {
+      expect(() => {
+        const params = {
+          menuElement,
+        };
+        params[selector] = 123;
+        const menu = new BaseMenu(params);
+
+        initializeMenu(menu);
+      }).toThrow(
+        `AccessibleMenu: ${selector} must be a valid CSS selector. "123" given.`
+      );
+    }
+  );
+
+  test("won't initialize if submenuItemSelector is provided and submenuToggleSelector isn't a CSS selector", () => {
+    expect(() => {
+      const menu = new BaseMenu({
+        menuElement,
+        submenuItemSelector: "li.dropdown",
+        submenuToggleSelector: 123,
+      });
+
+      initializeMenu(menu);
+    }).toThrow(
+      'AccessibleMenu: submenuToggleSelector must be a valid CSS selector. "123" given.'
+    );
+  });
+
+  test("won't initialize if submenuItemSelector is provided and submenuSelector isn't a CSS selector", () => {
+    expect(() => {
+      const menu = new BaseMenu({
+        menuElement,
+        submenuItemSelector: "li.dropdown",
+        submenuSelector: 123,
+      });
+
+      initializeMenu(menu);
+    }).toThrow(
+      'AccessibleMenu: submenuSelector must be a valid CSS selector. "123" given.'
+    );
+  });
+
+  test("will initialize if controllerElement and containerElement are HTMLELements", () => {
+    expect(() => {
+      const menu = new BaseMenu({
+        menuElement,
+        containerElement,
+        controllerElement,
+      });
+
+      initializeMenu(menu);
+    }).not.toThrow(Error);
+  });
+
+  test("won't initialize if controllerElement is provided without containerElement", () => {
+    expect(() => {
+      const menu = new BaseMenu({
+        menuElement,
+        controllerElement,
+      });
+
+      initializeMenu(menu);
+    }).toThrow(
+      "AccessibleMenu: containerElement must be an instance of HTMLElement. object given."
+    );
+  });
+
+  test("won't initialize if containerElement is provided without containerElement", () => {
+    expect(() => {
+      const menu = new BaseMenu({
+        menuElement,
+        containerElement,
+      });
+
+      initializeMenu(menu);
+    }).toThrow(
+      "AccessibleMenu: controllerElement must be an instance of HTMLElement. object given."
+    );
+  });
+
+  test("won't initialize if controllerElement is isn't an HTMLElement", () => {
+    expect(() => {
+      const menu = new BaseMenu({
+        menuElement,
+        controllerElement: 123,
+        containerElement,
+      });
+
+      initializeMenu(menu);
+    }).toThrow(
+      "AccessibleMenu: controllerElement must be an instance of HTMLElement. number given."
+    );
+  });
+
+  test("won't initialize if containerElement is isn't an HTMLElement", () => {
+    expect(() => {
+      const menu = new BaseMenu({
+        menuElement,
+        controllerElement,
+        containerElement: 123,
+      });
+
+      initializeMenu(menu);
+    }).toThrow(
+      "AccessibleMenu: containerElement must be an instance of HTMLElement. number given."
+    );
+  });
+
+  test.each(classLists)(
+    "won't initialize if %s is not a valid class list",
+    (classList) => {
+      expect(() => {
+        const params = {
+          menuElement,
+        };
+        params[classList] = 123;
+
+        const menu = new BaseMenu(params);
+
+        initializeMenu(menu);
+      }).toThrow(
+        `AccessibleMenu: ${classList} must be a string or an array of strings. number given.`
+      );
+      expect(() => {
+        const params = {
+          menuElement,
+        };
+        params[classList] = [123, "class"];
+
+        const menu = new BaseMenu(params);
+
+        initializeMenu(menu);
+      }).toThrow(
+        `AccessibleMenu: ${classList} must be a string or an array of strings. An array containing non-strings given.`
+      );
+    }
+  );
+
+  test("won't initialize if isTopLevel isn't a boolean", () => {
+    expect(() => {
+      const menu = new BaseMenu({
+        menuElement,
+        isTopLevel: 123,
+      });
+
+      initializeMenu(menu);
+    }).toThrow("AccessibleMenu: isTopLevel must be a boolean. number given.");
+  });
+
+  test("will initialize if parentMenu a BaseMenu", () => {
+    expect(() => {
+      const parentMenu = new BaseMenu({ menuElement });
+      const menu = new BaseMenu({
+        menuElement,
+        parentMenu,
+      });
+
+      initializeMenu(menu);
+    }).not.toThrow(Error);
+  });
+
+  test("won't initialize if parentMenu isn't a BaseMenu or null", () => {
+    expect(() => {
+      const menu = new BaseMenu({
+        menuElement,
+        parentMenu: "parent menu",
+      });
+
+      initializeMenu(menu);
+    }).toThrow(
+      "AccessibleMenu: parentMenu must be an instance of BaseMenu. string given."
+    );
+  });
+
+  test("won't initialize if hoverType is invalid", () => {
+    expect(() => {
+      const menu = new BaseMenu({
+        menuElement,
+        hoverType: "fake",
+      });
+
+      initializeMenu(menu);
+    }).toThrow(
+      'AccessibleMenu: hoverType must be one of the following values: off, on, dynamic. "fake" given.'
+    );
+  });
+
+  test("won't initialize if hoverDelay is invalid", () => {
+    expect(() => {
+      const menu = new BaseMenu({
+        menuElement,
+        hoverDelay: "250",
+      });
+
+      initializeMenu(menu);
+    }).toThrow("AccessibleMenu: hoverDelay must be a number. string given.");
   });
 });
