6605b9140d1be1d7b2440650f3e0469074523961
Nick Milton <nickdjm@gmail.com>
2021-06-30 11:05:44 -0400

    docs: fix formatting and config

--- jsdoc.config.js
+++ jsdoc.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   source: {
-    excludePattern: "",
+    include: ["src", "README.md"],
+    includePattern: ".js$",
+    excludePattern: "(node_modules/|docs)",
+  },
+  plugins: ["plugins/markdown"],
+  opts: {
+    destination: "./docs/dist",
+    encoding: "utf8",
+    private: true,
+    recurse: true,
   },
 };
