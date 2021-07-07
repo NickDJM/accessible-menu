c6845b2d6c36cde79f7bebf24968b1ed849ef1e9
Nick Milton <nickdjm@gmail.com>
2021-07-07 15:24:56 -0400

    chore: run build

--- dist/menubar.esm.js
+++ dist/menubar.esm.js
@@ -2525,8 +2525,10 @@ var Menubar = /*#__PURE__*/function (_BaseMenu) {
    * {@link Menubar#handleKeydown|keydown}, and
    * {@link Menubar#handleKeyup|keyup} events for the menu.
    *
-   * This will also set the menu's `role` to "menubar" and the first menu item's
-   * `tabIndex` to 0 in the DOM.
+   * This will also set the menu's `role` to "menubar" in the DOM.
+   *
+   * If the menu is a root menu the first menu item's `tabIndex` will be set to
+   * 0 in the DOM.
    *
    * If the BaseMenu's initialize method throws an error,
    * this will catch it and log it to the console.
