ec16bc52c2d8719403f7955ce6a88caa28403564
Nick Milton <nickdjm@gmail.com>
2023-02-06 12:17:07 -0500

    refactor(validation): return a status and message when validation fails
    BREAKING CHANGE: changes the return type of validation functions from boolean to object

--- tests/validate/isValidClassList.test.js
+++ tests/validate/isValidClassList.test.js
@@ -5,37 +5,65 @@
 import { isValidClassList } from "../../src/validate";
 
 describe("isValidClassList", () => {
-  // Mock console.error.
-  console.error = jest.fn();
-
   const singleClass = "class";
   const multipleClasses = ["class", "other-class"];
 
   test("returns true when passed a single string", () => {
-    expect(isValidClassList({ singleClass })).toBeTrue();
+    const check = isValidClassList({ singleClass });
+
+    expect(check.status).toBeTrue();
+    expect(check.error).toBeNull();
   });
 
   test("returns true when passed an array of strings", () => {
-    expect(isValidClassList({ multipleClasses })).toBeTrue();
+    const check = isValidClassList({ multipleClasses });
+
+    expect(check.status).toBeTrue();
+    expect(check.error).toBeNull();
   });
 
   test("returns false when passed a single number", () => {
-    expect(isValidClassList({ classes: 123 })).toBeFalse();
+    const check = isValidClassList({ classes: 123 });
+
+    expect(check.status).toBeFalse();
+    expect(check.error.message).toBe(
+      "classes must be a string or an array of strings. number given."
+    );
   });
 
   test("returns false when passed an array of numbers", () => {
-    expect(isValidClassList({ classes: [123, 321] })).toBeFalse();
+    const check = isValidClassList({ classes: [123, 321] });
+
+    expect(check.status).toBeFalse();
+    expect(check.error.message).toBe(
+      "classes must be a string or an array of strings. An array containing non-strings given."
+    );
   });
 
   test("returns false when passed an array of strings and numbers", () => {
-    expect(isValidClassList({ classes: ["class", 123] })).toBeFalse();
+    const check = isValidClassList({ classes: ["class", 123] });
+
+    expect(check.status).toBeFalse();
+    expect(check.error.message).toBe(
+      "classes must be a string or an array of strings. An array containing non-strings given."
+    );
   });
 
   test("returns false when passed a string that is _not_ contained in an object", () => {
-    expect(isValidClassList(singleClass)).toBeFalse();
+    const check = isValidClassList(singleClass);
+
+    expect(check.status).toBeFalse();
+    expect(check.error.message).toBe(
+      'Values given to isValidClassList() must be inside of an object. "string" given.'
+    );
   });
 
   test("returns false when passed an array of strings that is _not_ contained in an object", () => {
-    expect(isValidClassList(multipleClasses)).toBeFalse();
+    const check = isValidClassList(multipleClasses);
+
+    expect(check.status).toBeFalse();
+    expect(check.error.message).toBe(
+      'Values given to isValidClassList() must be inside of an object. "object" given.'
+    );
   });
 });
--- tests/validate/isValidEvent.test.js
+++ tests/validate/isValidEvent.test.js
@@ -5,21 +5,31 @@
 import { isValidEvent } from "../../src/validate";
 
 describe("isValidEvent", () => {
-  // Mock console.error.
-  console.error = jest.fn();
-
   // Valid event options.
   const validEvents = ["none", "mouse", "keyboard", "character"];
 
   test.each(validEvents)("returns true if '%s' is passed", (event) => {
-    expect(isValidEvent({ event })).toBeTrue();
+    const check = isValidEvent({ event });
+
+    expect(check.status).toBeTrue();
+    expect(check.error).toBeNull();
   });
 
   test("returns false if an unsupported event is passed", () => {
-    expect(isValidEvent({ event: "unsupported" })).toBeFalse();
+    const check = isValidEvent({ event: "unsupported" });
+
+    expect(check.status).toBeFalse();
+    expect(check.error.message).toBe(
+      'event must be one of the following values: none, mouse, keyboard, character. "unsupported" given.'
+    );
   });
 
   test("returns false if a supported event, _not_ in an object, is passed", () => {
-    expect(isValidEvent("none")).toBeFalse();
+    const check = isValidEvent("none");
+
+    expect(check.status).toBeFalse();
+    expect(check.error.message).toBe(
+      'Values given to isValidEvent() must be inside of an object. "string" given.'
+    );
   });
 });
