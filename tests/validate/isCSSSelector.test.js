8933f444372ffce740a98c2124ac6f169bb9281e
Nick Milton <nickdjm@gmail.com>
2023-02-06 12:17:07 -0500

    refactor(validation): return a status and message when validation fails
    BREAKING CHANGE: changes the return type of validation functions from boolean to object

--- tests/validate/isCSSSelector.test.js
+++ tests/validate/isCSSSelector.test.js
@@ -5,18 +5,28 @@
 import { isCSSSelector } from "../../src/validate";
 
 describe("isCSSSelector", () => {
-  // Mock console.error.
-  console.error = jest.fn();
-
   test("returns true when passed a string", () => {
-    expect(isCSSSelector({ selector: "div" })).toBeTrue();
+    const check = isCSSSelector({ selector: "div" });
+
+    expect(check.status).toBeTrue();
+    expect(check.error).toBeNull();
   });
 
   test("returns false when passed a number", () => {
-    expect(isCSSSelector({ selector: 123 })).toBeFalse();
+    const check = isCSSSelector({ selector: 123 });
+
+    expect(check.status).toBeFalse();
+    expect(check.error.message).toBe(
+      'selector must be a valid CSS selector. "123" given.'
+    );
   });
 
   test("returns false when passed a string that is _not_ in an object", () => {
-    expect(isCSSSelector("div")).toBeFalse();
+    const check = isCSSSelector("div");
+
+    expect(check.status).toBeFalse();
+    expect(check.error.message).toBe(
+      'Values given to isCSSSelector() must be inside of an object. "string" given.'
+    );
   });
 });
