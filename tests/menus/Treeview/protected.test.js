a617c60b542fa3c35b975c948debed3c20678b42
Nick Milton <nickdjm@gmail.com>
2023-06-13 13:30:28 -0400

    test(protected): update test descriptions and add todos

--- tests/menus/Treeview/protected.test.js
+++ tests/menus/Treeview/protected.test.js
@@ -1,5 +1,9 @@
 /**
  * Tests for protected methods of Treeview class.
+ *
+ * todo: Add tests for: _validate(), _setDOMElements(), _findRootMenu(),
+ * _createChildElements(), _handleFocus(), _handleClick(), _handelHover(),
+ * _handleKeydown(), and _handleKeyup().
  */
 
 import { describe, it, expect, beforeEach, afterEach } from "vitest";
@@ -14,35 +18,20 @@ afterEach(() => {
   document.body.innerHTML = "";
 });
 
-// Test Treeview _setDOMElementType.
-describe("Treeview._setDOMElementType", () => {
-  const allowedTypes = [
-    "menuItems",
-    "submenuItems",
-    "submenuToggles",
-    "submenus",
-  ];
-  const disallowedTypes = ["menuLinks"];
+// Test Treeview protected methods.
+describe("Treeview protected methods", () => {
+  // Test Treeview _setDOMElementType().
+  describe("_setDOMElementType", () => {
+    const allowedTypes = [
+      "menuItems",
+      "submenuItems",
+      "submenuToggles",
+      "submenus",
+    ];
+    const disallowedTypes = ["menuLinks"];
 
-  // Test that all allowed types can be set.
-  it.each(allowedTypes)("should set the %s element type", (elementType) => {
-    // Create a new Treeview instance for testing.
-    const menu = new Treeview({
-      menuElement: document.querySelector("ul"),
-      submenuItemSelector: "li.dropdown",
-      containerElement: document.querySelector("nav"),
-      controllerElement: document.querySelector("button"),
-    });
-
-    expect(() => {
-      menu._setDOMElementType(elementType);
-    }).not.toThrow();
-  });
-
-  // Test that all disallowed types throw an error.
-  it.each(disallowedTypes)(
-    "should throw an error when setting the %s element type",
-    (elementType) => {
+    // Test that all allowed types can be set.
+    it.each(allowedTypes)("should set the %s element type", (elementType) => {
       // Create a new Treeview instance for testing.
       const menu = new Treeview({
         menuElement: document.querySelector("ul"),
@@ -53,57 +42,57 @@ describe("Treeview._setDOMElementType", () => {
 
       expect(() => {
         menu._setDOMElementType(elementType);
-      }).toThrow(
-        `The "${elementType}" element cannot be set through _setDOMElementType`
-      );
-    }
-  );
-
-  // Test that an invalid type throws an error.
-  it("should throw an error when setting an invalid element type", () => {
-    // Create a new Treeview instance for testing.
-    const menu = new Treeview({
-      menuElement: document.querySelector("ul"),
-      submenuItemSelector: "li.dropdown",
-      containerElement: document.querySelector("nav"),
-      controllerElement: document.querySelector("button"),
+      }).not.toThrow();
     });
 
-    expect(() => {
-      menu._setDOMElementType("menu");
-    }).toThrow('"menu" is not a valid element type within the menu.');
-  });
-});
+    // Test that all disallowed types throw an error.
+    it.each(disallowedTypes)(
+      "should throw an error when setting the %s element type",
+      (elementType) => {
+        // Create a new Treeview instance for testing.
+        const menu = new Treeview({
+          menuElement: document.querySelector("ul"),
+          submenuItemSelector: "li.dropdown",
+          containerElement: document.querySelector("nav"),
+          controllerElement: document.querySelector("button"),
+        });
 
-// Test Treeview _resetDOMElementType.
-describe("Treeview._resetDOMElementType", () => {
-  const allowedTypes = [
-    "menuItems",
-    "submenuItems",
-    "submenuToggles",
-    "submenus",
-  ];
-  const disallowedTypes = ["menu", "container", "controller"];
+        expect(() => {
+          menu._setDOMElementType(elementType);
+        }).toThrow(
+          `The "${elementType}" element cannot be set through _setDOMElementType`
+        );
+      }
+    );
 
-  // Test that all allowed types can be reset.
-  it.each(allowedTypes)("should reset the %s element type", (elementType) => {
-    // Create a new Treeview instance for testing.
-    const menu = new Treeview({
-      menuElement: document.querySelector("ul"),
-      submenuItemSelector: "li.dropdown",
-      containerElement: document.querySelector("nav"),
-      controllerElement: document.querySelector("button"),
+    // Test that an invalid type throws an error.
+    it("should throw an error when setting an invalid element type", () => {
+      // Create a new Treeview instance for testing.
+      const menu = new Treeview({
+        menuElement: document.querySelector("ul"),
+        submenuItemSelector: "li.dropdown",
+        containerElement: document.querySelector("nav"),
+        controllerElement: document.querySelector("button"),
+      });
+
+      expect(() => {
+        menu._setDOMElementType("menu");
+      }).toThrow('"menu" is not a valid element type within the menu.');
     });
-
-    expect(() => {
-      menu._resetDOMElementType(elementType);
-    }).not.toThrow();
   });
 
-  // Test that all disallowed types throw an error.
-  it.each(disallowedTypes)(
-    "should throw an error when resetting the %s element type",
-    (elementType) => {
+  // Test Treeview _resetDOMElementType().
+  describe("_resetDOMElementType", () => {
+    const allowedTypes = [
+      "menuItems",
+      "submenuItems",
+      "submenuToggles",
+      "submenus",
+    ];
+    const disallowedTypes = ["menu", "container", "controller"];
+
+    // Test that all allowed types can be reset.
+    it.each(allowedTypes)("should reset the %s element type", (elementType) => {
       // Create a new Treeview instance for testing.
       const menu = new Treeview({
         menuElement: document.querySelector("ul"),
@@ -114,24 +103,42 @@ describe("Treeview._resetDOMElementType", () => {
 
       expect(() => {
         menu._resetDOMElementType(elementType);
-      }).toThrow(
-        `The "${elementType}" element cannot be reset through _resetDOMElementType`
-      );
-    }
-  );
-
-  // Test that an invalid type throws an error.
-  it("should throw an error when resetting an invalid element type", () => {
-    // Create a new Treeview instance for testing.
-    const menu = new Treeview({
-      menuElement: document.querySelector("ul"),
-      submenuItemSelector: "li.dropdown",
-      containerElement: document.querySelector("nav"),
-      controllerElement: document.querySelector("button"),
+      }).not.toThrow();
     });
 
-    expect(() => {
-      menu._resetDOMElementType("menuLinks");
-    }).toThrow('"menuLinks" is not a valid element type within the menu.');
+    // Test that all disallowed types throw an error.
+    it.each(disallowedTypes)(
+      "should throw an error when resetting the %s element type",
+      (elementType) => {
+        // Create a new Treeview instance for testing.
+        const menu = new Treeview({
+          menuElement: document.querySelector("ul"),
+          submenuItemSelector: "li.dropdown",
+          containerElement: document.querySelector("nav"),
+          controllerElement: document.querySelector("button"),
+        });
+
+        expect(() => {
+          menu._resetDOMElementType(elementType);
+        }).toThrow(
+          `The "${elementType}" element cannot be reset through _resetDOMElementType`
+        );
+      }
+    );
+
+    // Test that an invalid type throws an error.
+    it("should throw an error when resetting an invalid element type", () => {
+      // Create a new Treeview instance for testing.
+      const menu = new Treeview({
+        menuElement: document.querySelector("ul"),
+        submenuItemSelector: "li.dropdown",
+        containerElement: document.querySelector("nav"),
+        controllerElement: document.querySelector("button"),
+      });
+
+      expect(() => {
+        menu._resetDOMElementType("menuLinks");
+      }).toThrow('"menuLinks" is not a valid element type within the menu.');
+    });
   });
 });
