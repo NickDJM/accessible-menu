d0cb9d7a984e58f5697e55d01e7391e133ac6846
Nick Milton <nickdjm@gmail.com>
2023-06-19 15:08:51 -0400

    test(keydown): add full keydown tests for treeview

--- tests/menus/Treeview/keyboard.test.js
+++ tests/menus/Treeview/keyboard.test.js
@@ -17,6 +17,307 @@ afterEach(() => {
   document.body.innerHTML = "";
 });
 
+// Test keyboard events on the Treeview.
+describe("Treeview", () => {
+  // Test keydown.
+  describe("keydown", () => {
+    // Test that keydown events set the current event to keyboard.
+    it("should set the current event to keyboard on keydown events", () => {
+      // Create a new Treeview instance for testing.
+      const menu = new Treeview({
+        menuElement: document.querySelector("ul"),
+        submenuItemSelector: "li.dropdown",
+        containerElement: document.querySelector("nav"),
+        controllerElement: document.querySelector("button"),
+      });
+
+      // Trigger a keydown event.
+      simulateKeyboardEvent("keydown", menu.dom.menu);
+
+      expect(menu.currentEvent).toBe("keyboard");
+    });
+
+    // Test Tab.
+    describe("Tab", () => {
+      // Test that blur is called on the menu on Tab keydown events if focus state is not none.
+      it("should call blur on the menu on Tab keydown events if focus state is not none", () => {
+        // Create a new Treeview instance for testing.
+        const menu = new Treeview({
+          menuElement: document.querySelector("ul"),
+          submenuItemSelector: "li.dropdown",
+          containerElement: document.querySelector("nav"),
+          controllerElement: document.querySelector("button"),
+        });
+
+        // Set up the menu.
+        menu.focusState = "self";
+
+        // Spy on the menu's blur method.
+        vi.spyOn(menu, "blur");
+
+        // Trigger a keydown event.
+        simulateKeyboardEvent("keydown", menu.dom.menu, {
+          key: "Tab",
+        });
+
+        expect(menu.blur).toHaveBeenCalled();
+      });
+
+      // Test that blur is not called on the menu on Tab keydown events if focus state is none.
+      it("should not call blur on the menu on Tab keydown events if focus state is none", () => {
+        // Create a new Treeview instance for testing.
+        const menu = new Treeview({
+          menuElement: document.querySelector("ul"),
+          submenuItemSelector: "li.dropdown",
+          containerElement: document.querySelector("nav"),
+          controllerElement: document.querySelector("button"),
+        });
+
+        // Spy on the menu's blur method.
+        vi.spyOn(menu, "blur");
+
+        // Trigger a keydown event.
+        simulateKeyboardEvent("keydown", menu.dom.menu, {
+          key: "Tab",
+        });
+
+        expect(menu.blur).not.toHaveBeenCalled();
+      });
+
+      // Test that focus is called on the menu on Tab keydown events if focus state is none.
+      it("should call focus on the menu on Tab keydown events if focus state is none", () => {
+        // Create a new Treeview instance for testing.
+        const menu = new Treeview({
+          menuElement: document.querySelector("ul"),
+          submenuItemSelector: "li.dropdown",
+          containerElement: document.querySelector("nav"),
+          controllerElement: document.querySelector("button"),
+        });
+
+        // Spy on the menu's focus method.
+        vi.spyOn(menu, "focus");
+
+        // Trigger a keydown event.
+        simulateKeyboardEvent("keydown", menu.dom.menu, {
+          key: "Tab",
+        });
+
+        expect(menu.focus).toHaveBeenCalled();
+      });
+
+      // Test that focus is not called on the menu on Tab keydown events if focus state is not none.
+      it("should not call focus on the menu on Tab keydown events if focus state is not none", () => {
+        // Create a new Treeview instance for testing.
+        const menu = new Treeview({
+          menuElement: document.querySelector("ul"),
+          submenuItemSelector: "li.dropdown",
+          containerElement: document.querySelector("nav"),
+          controllerElement: document.querySelector("button"),
+        });
+
+        // Set up the menu.
+        menu.focusState = "self";
+
+        // Spy on the menu's focus method.
+        vi.spyOn(menu, "focus");
+
+        // Trigger a keydown event.
+        simulateKeyboardEvent("keydown", menu.dom.menu, {
+          key: "Tab",
+        });
+
+        expect(menu.focus).not.toHaveBeenCalled();
+      });
+    });
+
+    // Test Spacebar, ArrowUp, ArrowDown, ArrowLeft, Asterisk, Home, and End.
+    describe.each([
+      "Spacebar",
+      "ArrowUp",
+      "ArrowDown",
+      "ArrowLeft",
+      "*",
+      "Home",
+      "End",
+    ])("%s", (key) => {
+      // Test that the event is prevented on keydown events if focus state is self.
+      it("should prevent the event on %s keydown events if focus state is self", () => {
+        // Create a new Treeview instance for testing.
+        const menu = new Treeview({
+          menuElement: document.querySelector("ul"),
+          submenuItemSelector: "li.dropdown",
+          containerElement: document.querySelector("nav"),
+          controllerElement: document.querySelector("button"),
+        });
+
+        // Set up the menu.
+        menu.focusState = "self";
+
+        // Trigger a keydown event.
+        const event = simulateKeyboardEvent("keydown", menu.dom.menu, {
+          key,
+        });
+
+        expect(event.defaultPrevented).toBeTruthy();
+      });
+    });
+
+    // Test Enter and ArrowRight.
+    describe.each(["Enter", "ArrowRight"])("%s", (key) => {
+      // Test that the event is prevented on keydown events if focus state is self and the current menu item is a submenu item.
+      it("should prevent the event on %s keydown events if focus state is self and the current menu item is a submenu item", () => {
+        // Create a new Treeview instance for testing.
+        const menu = new Treeview({
+          menuElement: document.querySelector("ul"),
+          submenuItemSelector: "li.dropdown",
+          containerElement: document.querySelector("nav"),
+          controllerElement: document.querySelector("button"),
+        });
+
+        // Set up the menu.
+        menu.focusState = "self";
+        menu.currentChild = 1;
+
+        // Trigger a keydown event.
+        const event = simulateKeyboardEvent("keydown", menu.dom.menu, {
+          key,
+        });
+
+        expect(event.defaultPrevented).toBeTruthy();
+      });
+
+      // Test that the event is not prevented on keydown events if focus state is self and the current menu item is not a submenu item.
+      it("should not prevent the event on %s keydown events if focus state is self and the current menu item is not a submenu item", () => {
+        // Create a new Treeview instance for testing.
+        const menu = new Treeview({
+          menuElement: document.querySelector("ul"),
+          submenuItemSelector: "li.dropdown",
+          containerElement: document.querySelector("nav"),
+          controllerElement: document.querySelector("button"),
+        });
+
+        // Set up the menu.
+        menu.focusState = "self";
+        menu.currentChild = 0;
+
+        // Trigger a keydown event.
+        const event = simulateKeyboardEvent("keydown", menu.dom.menu, {
+          key,
+        });
+
+        expect(event.defaultPrevented).toBeFalsy();
+      });
+
+      // Test that the event is not prevented on keydown events if focus state is not self and the current menu item is a submenu item.
+      it("should not prevent the event on %s keydown events if focus state is not self and the current menu item is a submenu item", () => {
+        // Create a new Treeview instance for testing.
+        const menu = new Treeview({
+          menuElement: document.querySelector("ul"),
+          submenuItemSelector: "li.dropdown",
+          containerElement: document.querySelector("nav"),
+          controllerElement: document.querySelector("button"),
+        });
+
+        // Set up the menu.
+        menu.focusState = "none";
+        menu.currentChild = 1;
+
+        // Trigger a keydown event.
+        const event = simulateKeyboardEvent("keydown", menu.dom.menu, {
+          key,
+        });
+
+        expect(event.defaultPrevented).toBeFalsy();
+      });
+    });
+
+    // Test Escape.
+    describe("Escape", () => {
+      // Test that the event is prevented on Escape keydown events if the focus state is self and the menu has a controller.
+      it("should prevent the Escape keydown event if the focus state is self and the menu has a controller", () => {
+        // Create a new Treeview instance for testing.
+        const menu = new Treeview({
+          menuElement: document.querySelector("ul"),
+          submenuItemSelector: "li.dropdown",
+          containerElement: document.querySelector("nav"),
+          controllerElement: document.querySelector("button"),
+        });
+
+        // Set up the menu.
+        menu.focusState = "self";
+        menu.currentChild = 0;
+
+        // Trigger a keydown event.
+        const event = simulateKeyboardEvent("keydown", menu.dom.menu, {
+          key: "Escape",
+        });
+
+        expect(event.defaultPrevented).toBeTruthy();
+      });
+
+      // Test that the event is not prevented on Escape keydown events if the focus state is not self and the menu has a controller.
+      it("should not prevent the Escape keydown event if the focus state is not self and the menu has a controller", () => {
+        // Create a new Treeview instance for testing.
+        const menu = new Treeview({
+          menuElement: document.querySelector("ul"),
+          submenuItemSelector: "li.dropdown",
+          containerElement: document.querySelector("nav"),
+          controllerElement: document.querySelector("button"),
+        });
+
+        // Set up the menu.
+        menu.currentChild = 0;
+
+        // Trigger a keydown event.
+        const event = simulateKeyboardEvent("keydown", menu.dom.menu, {
+          key: "Escape",
+        });
+
+        expect(event.defaultPrevented).toBeFalsy();
+      });
+
+      // Test that the event is not prevented on Escape keydown events if the focus state is self and the menu does not have a controller.
+      it("should not prevent the Escape keydown event if the focus state is self and the menu does not have a controller", () => {
+        // Create a new Treeview instance for testing.
+        const menu = new Treeview({
+          menuElement: document.querySelector("ul"),
+          submenuItemSelector: "li.dropdown",
+        });
+
+        // Set up the menu.
+        menu.focusState = "self";
+        menu.currentChild = 0;
+
+        // Trigger a keydown event.
+        const event = simulateKeyboardEvent("keydown", menu.dom.menu, {
+          key: "Escape",
+        });
+
+        expect(event.defaultPrevented).toBeFalsy();
+      });
+    });
+  });
+
+  // Test keyup.
+  describe("keyup", () => {
+    // Test that keyup events set the current event to keyboard.
+    it("should set the current event to keyboard on keyup events", () => {
+      // Create a new Treeview instance for testing.
+      const menu = new Treeview({
+        menuElement: document.querySelector("ul"),
+        submenuItemSelector: "li.dropdown",
+        containerElement: document.querySelector("nav"),
+        controllerElement: document.querySelector("button"),
+      });
+
+      // Trigger a keyup event on the menu's controller.
+      simulateKeyboardEvent("keyup", menu.dom.menu);
+
+      expect(menu.currentEvent).toBe("keyboard");
+    });
+  });
+});
+
 // Test keyboard events on the Treeview's controller.
 describe("Treeview Controller", () => {
   // Test keydown.
