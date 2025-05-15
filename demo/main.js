15ef65816252b3957a5f97a0f33163cc5d34f1e7
Nick Milton <git@ndjm.ca>
2025-05-14 22:03:25 -0400

    chore: remove temp console log

--- demo/main.js
+++ demo/main.js
@@ -95,12 +95,6 @@ function generateMenu() {
     controllerElement: nav.querySelector("button"),
     ...options,
   });
-
-  nav.querySelectorAll("a,button").forEach((element) => {
-    element.addEventListener("click", (event) => {
-      console.log("Clicked:", event.target);
-    });
-  });
 }
 
 // Set up menu structure buttons.
