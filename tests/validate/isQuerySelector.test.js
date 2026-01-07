e55c1c7e7a5c93934bf3fc27c1e91e4bcbbdef84
Nick Milton <git@ndjm.ca>
2026-01-07 17:46:00 +0000

    test(isQuerySelector): add tests for broken selectors

--- tests/validate/isQuerySelector.test.js
+++ tests/validate/isQuerySelector.test.js
@@ -27,7 +27,7 @@ describe("isQuerySelector", () => {
 
   // Test an invalid query selector.
   it("should return false when checking if an invalid query selector is a valid query selector", () => {
-    const selector = 1;
+    const selector = "div[";
     expect(() => {
       isQuerySelector({ selector });
     }).toThrow(TypeError);
@@ -35,6 +35,23 @@ describe("isQuerySelector", () => {
 
   // Test multiple invalid query selectors.
   it("should return false when checking if multiple invalid query selectors are valid query selectors", () => {
+    const selector1 = "div[";
+    const selector2 = "a[";
+    expect(() => {
+      isQuerySelector({ selector1, selector2 });
+    }).toThrow(TypeError);
+  });
+
+  // Test non-string query selector.
+  it("should return false when checking if a non-string query selector is a valid query selector", () => {
+    const selector = 1;
+    expect(() => {
+      isQuerySelector({ selector });
+    }).toThrow(TypeError);
+  });
+
+  // Test multiple non-string query selectors.
+  it("should return false when checking if multiple non-string query selectors are valid query selectors", () => {
     const selector1 = 1;
     const selector2 = 1;
     expect(() => {
@@ -42,8 +59,25 @@ describe("isQuerySelector", () => {
     }).toThrow(TypeError);
   });
 
+  // Test null.
+  it("should return false when checking if null is a valid query selector", () => {
+    const selector = null;
+    expect(() => {
+      isQuerySelector({ selector });
+    }).toThrow(TypeError);
+  });
+
   // Test a mixed valid and invalid query selector.
   it("should return false when checking if a mixed valid and invalid query selector is a valid query selector", () => {
+    const selector1 = "div";
+    const selector2 = "div[";
+    expect(() => {
+      isQuerySelector({ selector1, selector2 });
+    }).toThrow(TypeError);
+  });
+
+  // Test a mixed valid and non-string query selector.
+  it("should return false when checking if a mixed valid and non-string query selector is a valid query selector", () => {
     const selector1 = "div";
     const selector2 = 1;
     expect(() => {
